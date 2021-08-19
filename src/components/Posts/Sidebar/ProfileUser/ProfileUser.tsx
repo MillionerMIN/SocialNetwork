caa4110a5b3d7c4ab0f8422fa35762bce415c41f
Vladimir <vladimirlyn@gmail.com>
2021-08-19 08:03:38 +0300

    step_71 setting with setState, localState. Add event to change description

--- src/components/Posts/Sidebar/ProfileUser/ProfileUser.tsx
+++ src/components/Posts/Sidebar/ProfileUser/ProfileUser.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import s from './ProfileUser.module.scss';
 import profileImg from '../../../../img/profile/ProfileImg.jpg'
 import profilePhoto from '../../../../img/icons/user.png'
@@ -10,16 +10,29 @@ type ProfileUserPropsType = {
 }
 
 const ProfileUser = (props: ProfileUserPropsType) => {
-   if (!props.profile) {
+   
+   const [editMode, setEditMode] = useState<boolean>(false);
+   const { profile } = props;
+
+   const toggleEditMode = () => {
+      setEditMode(!editMode)
+   }
+
+   if (!profile) {
       return <Spinner />
    }
    return <div className={s.profile}>
       <img className={s.bg} src={profileImg} alt="profileImg" />
       <div>
-         <img className={s.photo} src={props.profile.photos.large !== null ? props.profile.photos.large : profilePhoto} alt="userPhoto" />
+         <img className={s.photo} src={profile.photos.large !== null ? profile.photos.large : profilePhoto} alt="userPhoto" />
       </div>
       <div className={s.title}>{props.profile?.fullName}</div>
-      <div className={s.descr}>{props.profile?.lookingForAJobDescription}</div>
+      {!editMode &&
+         <div onDoubleClick={toggleEditMode} className={s.descr}>{profile?.lookingForAJobDescription}</div>
+      }
+      {editMode &&
+         <input onBlur={toggleEditMode} autoFocus className={s.descr} value={profile?.lookingForAJobDescription} />
+      }
    </div>
 }
 
