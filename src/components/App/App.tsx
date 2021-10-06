e527c11ab4cdc56afaf334176468a2a5d99a1fb8
Vladimir Liankevich <vladimirlyn@gmail.com>
2021-10-06 13:41:03 +0300

    step_94 using React.lazy and Suspense

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,65 +1,79 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { Route } from 'react-router-dom';
 
 //import components
-import ChatsContainer from '../Chats/ChatsContainer';
-import PostsContainer from '../Posts/PostsContainer';
-import UsersContainer from '../Users/UsersContainer';
+
 import { Footer } from '../Footer/Footer';
-import ProfileUserContainer from '../Posts/Sidebar/ProfileUser/ProfileUserContainer';
-import LoginContainer from '../Login/LoginContainer';
+
 import NavigationContainer from '../Navigation/NavigationContainer';
 
-// import News from './components/News/News';
-// import Setting from './components/Setting/Setting';
-// import Other from './components/Other/Other';
 import { withRouter } from 'react-router';
 import { connect } from 'react-redux';
 import { compose } from 'redux';
 import { initializeApp } from '../../redux/app-reducer';
 import { AppStateType } from '../../redux/redux-store';
 import { Spinner } from '../common/Spinner/Spinner';
+import { withSuspense } from '../../hoc/withSuspense';
+
+//lazy import
+
+const ProfileUserContainer = React.lazy(
+  () => import('../Posts/Sidebar/ProfileUser/ProfileUserContainer')
+);
+const PostsContainer = React.lazy(() => import('../Posts/PostsContainer'));
+const ChatsContainer = React.lazy(() => import('../Chats/ChatsContainer'));
+const LoginContainer = React.lazy(() => import('../Login/LoginContainer'));
+const UsersContainer = React.lazy(() => import('../Users/UsersContainer'));
 
 interface AppPropsType extends React.Component {
-    initializeApp: () => void
-
+  initializeApp: () => void;
 }
 type mapStateToPropsType = {
-    initialized: boolean
-}
+  initialized: boolean;
+};
 
 class App extends React.Component<AppPropsType & mapStateToPropsType, {}> {
-    componentDidMount() {
-        this.props.initializeApp()
+  componentDidMount() {
+    this.props.initializeApp();
+  }
+  render() {
+    if (!this.props.initialized) {
+      return <Spinner />;
     }
-    render() {
-        if (!this.props.initialized) {
-            return <Spinner />
-        }
 
-        return (
-            <div>
-                <NavigationContainer />
-                <Route path={'/profile/:userId?'} render={() => <ProfileUserContainer />} />
-                <Route path={'/posts'} render={() => <PostsContainer />} />
-                <Route path={'/chats'} render={() => <ChatsContainer />} />
-                {/* <Route path={'/news'} render={() => <News />} /> */}
-                <Route path={'/users'} render={() => <UsersContainer />} />
-                <Route path={'/login'} render={() => <LoginContainer />} />
-                {/* <Route path={'/setting'} render={() => <Setting />} />
+    return (
+      <div>
+        <NavigationContainer />
+        <Route
+          path={'/profile/:userId?'}
+          render={() => {
+            return (
+              <Suspense fallback={<div>...Loading</div>}>
+                <ProfileUserContainer />
+              </Suspense>
+            );
+          }}
+        />
+        <Route path={'/posts'} render={withSuspense(PostsContainer)} />
+        <Route path={'/chats'} render={withSuspense(ChatsContainer)} />
+        {/* <Route path={'/news'} render={() => <News />} /> */}
+        <Route path={'/users'} render={withSuspense(UsersContainer)} />
+        <Route path={'/login'} render={withSuspense(LoginContainer)} />
+        {/* <Route path={'/setting'} render={() => <Setting />} />
             <Route path={'/other'} render={() => <Other />} /> */}
-                <Footer />
-            </div>
-        );
-    }
+        <Footer />
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = (state: AppStateType): mapStateToPropsType => {
-    return {
-        initialized: state.app.initialized
-    }
-}
+  return {
+    initialized: state.app.initialized,
+  };
+};
 
 export default compose<React.ComponentType>(
-    withRouter,
-    connect(mapStateToProps, { initializeApp }))(App);
+  withRouter,
+  connect(mapStateToProps, { initializeApp })
+)(App);
---
+++ src/hoc/withSuspense.tsx
@@ -0,0 +1,11 @@
+import { ComponentType, ReactNode, Suspense } from 'react';
+
+export function withSuspense<T>(ChildComp: ComponentType<T>) {
+  return (props: any) => {
+    return (
+      <Suspense fallback={<div>...Loading</div>}>
+        <ChildComp {...props} />
+      </Suspense>
+    );
+  };
+}
